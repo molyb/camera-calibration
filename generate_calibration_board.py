3b6865b6d55f8e3fb33dd46006026074ec9a59dc
molyb <8923029+molyb@users.noreply.github.com>
2025-10-18 18:40:29 +0900

    Fix generate function for considering dpi

--- generate_calibration_board.py
+++ generate_calibration_board.py
@@ -13,12 +13,10 @@ checker_size_mm = 22
 marker_size_mm = 16
 board_cell_x = 16
 board_cell_y = 12
-board_width_pixel = 1400
-board_height_pixel = 1050
 
 
-def create_canvas(width, height):
-    return np.full((height, width), 255, dtype=np.uint8)  # white
+def create_canvas(width_pixel, height_pixel):
+    return np.full((height_pixel, width_pixel), 255, dtype=np.uint8)  # white
 
 
 def merge(canvas, image):
@@ -37,14 +35,15 @@ def put_params(image_in):
     # e.g. "| 16 x 12 | Checker 22 mm | Marker 16 mm | Dictionary 4X4_250 |"
     text = f"| {board_cell_x} x {board_cell_y} | Checker {checker_size_mm} mm | Marker {marker_size_mm} mm | Dictionary 4X4_250 |"
     font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 0.7
-    thickness = 1
+    font_scale = image_in.shape[0] * 0.0006
+    thickness = round(font_scale * 0.75)  # font_scaleを丸めるのはこの計算の後
+    font_scale = int(font_scale)
     color = (0, 0, 0)  # black
-
+    print(image_in.shape)
     image_out = image_in.copy()
 
     # 左下の座標を計算（余白を入れる）
-    tolerance_pixel = 15
+    tolerance_pixel = int(image_out.shape[0] * 0.01)
     x = tolerance_pixel
     y = image_out.shape[0] - tolerance_pixel  # 画像下端から n [pixel] 上
 
@@ -54,21 +53,21 @@ def put_params(image_in):
     return image_out
 
 
-def generate_board_image():
+def generate_board_image(width_pixel: int, height_pixel: int, dpi: int):
     dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
     board = cv2.aruco.CharucoBoard(
         size=(board_cell_x, board_cell_y),
-        squareLength=checker_size_mm / 1000.0,
-        markerLength=marker_size_mm / 1000.0,
+        squareLength=checker_size_mm,
+        markerLength=marker_size_mm,
         dictionary=dictionary,
     )
-    image = board.generateImage((board_width_pixel, board_height_pixel))
 
-    # board_width_pixel, board_height_pixel より canvas が大きくなるよう scale を設定
-    scale_mm_to_pixel = 4
-    canvas = create_canvas(
-        paper_width_mm * scale_mm_to_pixel, paper_height_mm * scale_mm_to_pixel
-    )
+    checker_size_pixel = checker_size_mm * dpi / 25.4
+    checker_size_width_pixel = int(checker_size_pixel * board_cell_x)
+    checker_size_height_pixel = int(checker_size_pixel * board_cell_y)
+    image = board.generateImage((checker_size_width_pixel, checker_size_height_pixel))
+
+    canvas = create_canvas(width_pixel, height_pixel)
     image = merge(canvas, image)
     image = put_params(image)
     return image
@@ -81,7 +80,12 @@ def main():
     )
     args = parser.parse_args()
 
-    image = generate_board_image()
+    dpi = int(600)
+    paper_width_inch = paper_width_mm / 25.4
+    paper_height_inch = paper_height_mm / 25.4
+    paper_width_pixel = int(dpi * paper_width_inch)
+    paper_height_pixel = int(dpi * paper_height_inch)
+    image = generate_board_image(paper_width_pixel, paper_height_pixel, dpi)
 
     if args.visualize:
         image_rgb = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
@@ -90,8 +94,6 @@ def main():
         plt.show()
 
     pil_img = Image.fromarray(image)
-    pixel_per_mm = image.shape[1] / paper_width_mm
-    dpi = 25.4 * pixel_per_mm
 
     # pil_img.save("calibration_board.png", dpi=(dpi, dpi))
     pil_img.save("calibration_board.pdf", dpi=(dpi, dpi))
